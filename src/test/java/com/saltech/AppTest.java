7d2064672bf694a4b801a08d3fd15629dd139da5
salimgbelim <salimgbelim@gmail.com>
2016-06-12 14:45:04 +0100

    integration Test

--- src/test/java/com/saltech/AppTest.java
+++ src/test/java/com/saltech/AppTest.java
@@ -1,38 +1,67 @@
 package com.saltech;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import com.google.common.io.Resources;
+import com.saltech.representations.Contact;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+import javax.ws.rs.core.MediaType;
+
+import java.io.File;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+
+    private Client client;
+
+    private Contact contactForTest = new Contact(0, "Jane", "Doe", "+987654321");
+
+    @ClassRule
+    public static final DropwizardAppRule<PhoneBookConfiguration> RULE =
+            new DropwizardAppRule<>(App.class,resourceFilePath("config.yml"));
+
+    @Before
+    public void SetUp(){
+        client = new Client();
+
+        HTTPBasicAuthFilter httpBasicAuthFilter = new HTTPBasicAuthFilter("salim_belim", "password");
+
+        client.addFilter(httpBasicAuthFilter);
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void createAndRetrieveContact(){
+
+        WebResource contactResource = client.resource("http://localhost:8080/contact");
+        ClientResponse response = contactResource
+                .type(MediaType.APPLICATION_JSON)
+                .post(ClientResponse.class, contactForTest);
+
+        String newContactURL = response.getHeaders().get("Location").get(0);
+        WebResource newContactResource = client.resource(newContactURL);
+        Contact contact = newContactResource.get(Contact.class);
+
+        assertThat(response.getStatus()).isEqualTo(201);
+        assertThat(contact.getFirstName()).isEqualTo(contactForTest.getFirstName());
+        assertThat(contact.getLastName()).isEqualTo(contactForTest.getLastName());
+        assertThat(contact.getPhone()).isEqualTo(contactForTest.getPhone());
+
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    public static String resourceFilePath(String resourceClassPathLocation) {
+        try {
+            return new File(Resources.getResource(resourceClassPathLocation).toURI()).getAbsolutePath();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
