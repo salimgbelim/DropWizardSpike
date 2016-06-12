36926016f1e8f145a651fc6628520d82086244c9
salimgbelim <salimgbelim@gmail.com>
2016-06-12 13:55:25 +0100

    rendering html view and stylesheet

--- src/main/java/com/saltech/App.java
+++ src/main/java/com/saltech/App.java
@@ -6,11 +6,13 @@ import com.saltech.resources.ContactResource;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
 import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.auth.basic.BasicAuthProvider;
 import io.dropwizard.client.JerseyClientBuilder;
 import io.dropwizard.jdbi.DBIFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import io.dropwizard.views.ViewBundle;
 import org.skife.jdbi.v2.DBI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,6 +30,8 @@ public class App extends Application<PhoneBookConfiguration> {
     @Override
     public void initialize(Bootstrap<PhoneBookConfiguration> bootstrap) {
 
+        bootstrap.addBundle(new ViewBundle());
+        bootstrap.addBundle(new AssetsBundle());
     }
 
     @Override
